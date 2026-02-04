80345c3c9326d57f8649ce67765d6972327b9cfa
Ratax397 <ifalyfaly84@gmail.com>
2026-02-04 11:06:16 +0300

    update

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -12,8 +12,8 @@ const Logo = () => {
         onClick={()=>router.push('/')}
         alt='logo'
         className='hidden md:block cursor-pointer'
-        height="100"
-        width="100"
+        height="95"
+        width="95"
         src="/images/logo.png"
     />
   )
